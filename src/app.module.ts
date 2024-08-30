a3187ea8ec84b33672c69df0c27de82be3f8ef4b
Ay-slim <ayooluwaadedipe@gmail.com>
2024-08-30 20:48:28 +0000

    Inject admin module into main app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/module';
 import { AuthModule } from './auth/module';
 import { ProductsModule } from './products/module';
+import { AdminModule } from './admin/module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     UsersModule,
     AuthModule,
+    AdminModule,
     ProductsModule,
   ],
 })
