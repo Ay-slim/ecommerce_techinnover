29ea47b4220f87aab37b154ecbe38eb0e6657d1b
Ay-slim <ayooluwaadedipe@gmail.com>
2024-08-30 22:57:56 +0000

    Already exists error constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,3 +3,4 @@ export const LOGIN_ERROR_MESSAGE = 'Error: Invalid username or password';
 export const ACCESS_DENIED_ERROR_MESSAGE = 'Error: Access denied';
 export const INTERNAL_SERVER_ERROR_MESSAGE = 'Something went wrong';
 export const BANNED_USER_ERROR_MESSAGE = 'Error: You have been banned, please contact admin'
+export const ALREADY_EXISTS_ERROR_MESSAGE = 'Error: Account already exists';
