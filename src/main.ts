d7dcb6d7d1c3c07c97cf96c255a4496c7e2fe62f
Ay-slim <ayooluwaadedipe@gmail.com>
2024-08-30 10:55:12 +0000

    App setup

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.enableCors({
+    origin: ['http://localhost:6969'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    credentials: false,
+  });
+  await app.listen(6969);
+  const appUrl = await app.getUrl()
+  console.log(`Application is running on: ${appUrl}`);
+}
+bootstrap();
