10d1bcfa923b76e51deaf535e4812f562bb9195d
Ay-slim <ayooluwaadedipe@gmail.com>
2024-08-30 15:47:58 +0000

    Add products module to main app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/module';
 import { AuthModule } from './auth/module';
+import { ProductsModule } from './products/module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     UsersModule,
     AuthModule,
+    ProductsModule,
   ],
 })
 export class AppModule {}
