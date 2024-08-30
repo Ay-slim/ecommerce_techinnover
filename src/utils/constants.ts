d145a9030114c8be0da4c9ee7c3871fd1b18a1de
Ay-slim <ayooluwaadedipe@gmail.com>
2024-08-30 21:14:53 +0000

    Separate banned user error message

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,4 @@ export const DEFAULT_FETCH_LIMIT = 50;
 export const LOGIN_ERROR_MESSAGE = 'Error: Invalid username or password';
 export const ACCESS_DENIED_ERROR_MESSAGE = 'Error: Access denied';
 export const INTERNAL_SERVER_ERROR_MESSAGE = 'Something went wrong';
+export const BANNED_USER_ERROR_MESSAGE = 'Error: You have been banned, please contact admin'
