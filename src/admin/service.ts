1cbe1d6fff2ccc3f2a36992c46b06e4373390b7b
Ay-slim <ayooluwaadedipe@gmail.com>
2024-08-30 20:46:19 +0000

    Keep admin service handy

---
+++ src/admin/service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/service';
+
+// TO BE DELETED AFTER SEEDING
+@Injectable()
+export class AdminService {
+  constructor(
+    private readonly userService: UsersService,
+  ) {}
+
+  // async seedSuperAdmin() {
+  //   await this.userService.create({
+  //     name: "superadmin",
+  //     email: "super@admin.com",
+  //     password: "admin",
+  //     role: "superadmin",
+  //   });
+  // }
+}
