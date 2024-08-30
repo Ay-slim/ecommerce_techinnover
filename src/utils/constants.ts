cb6415930d3ac4b02d0961d4a6292caf59141105
Ay-slim <ayooluwaadedipe@gmail.com>
2024-08-30 15:47:29 +0000

    Constant util update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
-export const DEFAULT_LIMIT = 50;
+export const DEFAULT_FETCH_LIMIT = 50;
 export const LOGIN_ERROR_MESSAGE = 'Invalid username or password';
 export const ACCESS_DENIED_ERROR_MESSAGE = 'Access denied';
+export const INTERNAL_SERVER_ERROR_MESSAGE = 'Something went wrong';
