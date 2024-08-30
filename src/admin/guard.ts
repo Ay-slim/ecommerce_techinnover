0255bd3e004eb79c1c107bdee53c1642e4f08a3a
Ay-slim <ayooluwaadedipe@gmail.com>
2024-08-30 20:48:12 +0000

    Admin and super admin guard

---
+++ src/admin/guard.ts
@@ -0,0 +1,24 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+} from '@nestjs/common';
+import { UserAuthDto } from '../auth/types';
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const { role }: UserAuthDto = request["info"];
+    return ["admin", "superadmin"].includes(role);
+  }
+}
+
+@Injectable()
+export class SuperAdminGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const { role }: UserAuthDto = request["info"];
+    return role === "superadmin";
+  }
+}
