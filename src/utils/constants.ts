b8116ccc1c28626b0ad86749ff4264e7406deabb
Ay-slim <ayooluwaadedipe@gmail.com>
2024-08-30 20:33:10 +0000

    Error message constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_FETCH_LIMIT = 50;
-export const LOGIN_ERROR_MESSAGE = 'Invalid username or password';
-export const ACCESS_DENIED_ERROR_MESSAGE = 'Access denied';
+export const LOGIN_ERROR_MESSAGE = 'Error: Invalid username or password';
+export const ACCESS_DENIED_ERROR_MESSAGE = 'Error: Access denied';
 export const INTERNAL_SERVER_ERROR_MESSAGE = 'Something went wrong';
