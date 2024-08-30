e9930440fabb8a354b2400d3e35f37833bc1d8a3
Ay-slim <ayooluwaadedipe@gmail.com>
2024-08-30 16:12:32 +0000

    Wrap auth endpoints in try catch

--- src/auth/controller.ts
+++ src/auth/controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post, Res, Get } from '@nestjs/common';
+import { Body, Controller, Post, Res, Get, BadRequestException, InternalServerErrorException } from '@nestjs/common';
 import { Response } from 'express';
 import { Public } from '../utils/publicRoutes';
 import { AuthService } from './service';
 import { LoginUserDto, RegisterUserDto } from './types';
+import { INTERNAL_SERVER_ERROR_MESSAGE } from 'src/utils/constants';
 
 @Controller('auth')
 export class AuthController {
@@ -14,27 +15,36 @@ export class AuthController {
     @Body() createUserDto: RegisterUserDto,
     @Res({ passthrough: true }) res?: Response,
   ) {
-    const {
-      name,
-      email,
-      password
-    } = createUserDto;
-    const userDetails = await this.authService.register({
-      name,
-      email,
-      password,
-    });
-    const {access_token, refresh_token} = await this.authService.generateTokens(userDetails);
-    res?.cookie('tokens', {
-      access_token,
-      refresh_token,
-    });
-    return {
-      data: userDetails,
-      message: "User created successfully",
-      success: true,
-      statusCode: 201,
-    };
+    try {
+        const {
+        name,
+        email,
+        password
+      } = createUserDto;
+      const userDetails = await this.authService.register({
+        name,
+        email,
+        password,
+      });
+      const {access_token, refresh_token} = await this.authService.generateTokens(userDetails);
+      res?.cookie('tokens', {
+        access_token,
+        refresh_token,
+      });
+      return {
+        data: userDetails,
+        message: "User created successfully",
+        success: true,
+        statusCode: 201,
+      };
+    } catch (e) {
+      console.log(e);
+      if (e?.message?.startsWith("Error: ")) {
+        throw new BadRequestException(e?.message);
+      } else {
+        throw new InternalServerErrorException(INTERNAL_SERVER_ERROR_MESSAGE);
+      }
+    }
   }
 
   @Public()
@@ -43,29 +53,47 @@ export class AuthController {
     @Body() loginUserDto: LoginUserDto,
     @Res({ passthrough: true }) res?: Response,
   ) {
-    const userDetails = await this.authService.login(loginUserDto);
-    const {access_token, refresh_token} = await this.authService.generateTokens(userDetails);
-    res?.cookie('tokens', {
-      access_token,
-      refresh_token,
-    });
-    return {
-      data: userDetails,
-      message: "Logged in successfully",
-      success: true,
-      statusCode: 201,
-    };
+    try {
+      const userDetails = await this.authService.login(loginUserDto);
+      const {access_token, refresh_token} = await this.authService.generateTokens(userDetails);
+      res?.cookie('tokens', {
+        access_token,
+        refresh_token,
+      });
+      return {
+        data: userDetails,
+        message: "Logged in successfully",
+        success: true,
+        statusCode: 201,
+      };
+    } catch (e) {
+      console.log(e);
+      if (e?.message?.startsWith("Error: ")) {
+        throw new BadRequestException(e?.message);
+      } else {
+        throw new InternalServerErrorException(INTERNAL_SERVER_ERROR_MESSAGE);
+      }
+    }
     
   }
 
   @Get('logout')
   async logout(@Res({ passthrough: true }) res: Response) {
-    res.clearCookie('tokens');
-    return {
-      data: null,
-      message: "Logged successfully",
-      success: true,
-      statusCode: 200,
-    };
+    try {
+      res.clearCookie('tokens');
+      return {
+        data: null,
+        message: "Logged successfully",
+        success: true,
+        statusCode: 200,
+      };
+    } catch (e) {
+      console.log(e);
+      if (e?.message?.startsWith("Error: ")) {
+        throw new BadRequestException(e?.message);
+      } else {
+        throw new InternalServerErrorException(INTERNAL_SERVER_ERROR_MESSAGE);
+      }
+    }
   }
 }
