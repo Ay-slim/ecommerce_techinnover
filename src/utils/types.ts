91762523aecbac3c9b957ca424b397efd5f7a539
Ay-slim <ayooluwaadedipe@gmail.com>
2024-08-30 15:47:14 +0000

    Response types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,4 +1,17 @@
+import { HttpStatus } from "@nestjs/common/enums";
+import { Product } from "src/products/interface";
+import { User } from "src/users/interface";
+
 export type PaginationDto = {
   page: number;
   limit: number;
+}
+
+export type MixedSchemaTypes = User | Product | Product[];
+
+export type ControllerReturnType = {
+  data: MixedSchemaTypes;
+  success: boolean;
+  message: string;
+  statusCode: HttpStatus
 }
