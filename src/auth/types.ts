84325b0884f79c7f96b77faedef60ce20d8419b3
Ay-slim <ayooluwaadedipe@gmail.com>
2024-08-30 11:11:34 +0000

    Auth types

---
+++ src/auth/types.ts
@@ -0,0 +1,25 @@
+export type UserAuthDto = {
+  name: string;
+  email: string;
+  banned: boolean;
+  role: string;
+  _id: string;
+  expiry: string;
+};
+
+export type LoginUserDto = {
+  email: string;
+  password: string;
+}
+
+export type RegisterUserDto = {
+  name: string
+} & LoginUserDto;
+
+export type AuthTokenDto ={
+  name: string;
+  email: string;
+  _id: string;
+  banned: boolean;
+  role: string
+}
