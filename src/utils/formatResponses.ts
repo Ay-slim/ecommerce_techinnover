608e3c4d802997f132a5da97ab0e8c1638761251
Ay-slim <ayooluwaadedipe@gmail.com>
2024-08-30 20:32:23 +0000

    Failure response util

--- src/utils/formatResponses.ts
+++ src/utils/formatResponses.ts
@@ -1,9 +1,10 @@
-import { HttpStatus } from "@nestjs/common";
+import { BadRequestException, HttpStatus, InternalServerErrorException } from "@nestjs/common";
 import { Product } from "src/products/interface";
 import { User } from "src/users/interface";
+import { INTERNAL_SERVER_ERROR_MESSAGE } from "./constants";
 
 export const successResponse = (
-  data: User | Product | Product[],
+  data: User | User[] | Product | Product[] | {name: string},
   message: string,
   statusCode: HttpStatus,
   success: boolean,
@@ -16,3 +17,14 @@ export const successResponse = (
   }
 }
 
+export const failureResponse = (
+  e: {
+    message: string;
+  },
+) => {
+  if (e?.message?.startsWith("Error: ")) {
+    throw new BadRequestException(e?.message);
+  } else {
+    throw new InternalServerErrorException(INTERNAL_SERVER_ERROR_MESSAGE);
+  }
+}
