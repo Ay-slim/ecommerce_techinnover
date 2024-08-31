4a35964779760381bfbf968a8e0e036d692138f0
Ay-slim <ayooluwaadedipe@gmail.com>
2024-08-31 00:01:50 +0000

    Implement rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { UsersModule } from './users/module';
 import { AuthModule } from './auth/module';
 import { ProductsModule } from './products/module';
 import { AdminModule } from './admin/module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -12,6 +13,10 @@ import { AdminModule } from './admin/module';
     AuthModule,
     AdminModule,
     ProductsModule,
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 100,
+    }]),
   ],
 })
 export class AppModule {}
--- src/users/module.ts
+++ src/users/module.ts
@@ -5,6 +5,8 @@ import { usersProviders } from './providers';
 import { DatabaseModule } from '../database/database.module';
 import { ProductsService } from 'src/products/service';
 import { productsProviders } from 'src/products/providers';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Module({
   imports: [DatabaseModule],
@@ -14,6 +16,10 @@ import { productsProviders } from 'src/products/providers';
     ...usersProviders,
     ProductsService,
     ...productsProviders,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    }    
   ],
 })
 export class UsersModule {}
